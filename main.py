156fd2ad456791817f298c349252589fadd06417
Maria Fernanda <mfernandasc1415@gmail.com>
2025-09-24 22:37:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from notebook import Notebook
+
+
+laptop = Notebook()
+laptop.mostrar_marca("Lenovo")
+laptop.mostrar_modelo("Ryzen 7")
+laptop.mostrar_informacoes("Lenovo", "Ryzen 7")
---
+++ notebook.py
@@ -0,0 +1,10 @@
+class Notebook:
+    def mostrar_marca(self, marca):
+        print(f"Sua marca é: {marca}")
+
+    def mostrar_modelo(self, modelo):
+        print(f"Seu modelo é: {modelo}")
+
+    def mostrar_informacoes(self, marca, modelo):
+        print(f"Seu Notebook é o {marca} {modelo}")
+
